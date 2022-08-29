b1eaa175c721ac1bf0bda6dad625b7592f08ec5e
Laxmi Yadav <91363406+yadavlaxmi@users.noreply.github.com>
2022-08-29 15:46:57 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,14 @@
+num=int(input("enter the number"))
+num1=int(input("enter the number"))
+while num<=num1:
+    i=1
+    while i<=10:
+        if num%2!=0:
+            b=i*num
+            print(b)
+        i=i+1
+    num=num+1
+
+    word = "bananana"
+i = word.find("na")
+print(i)
